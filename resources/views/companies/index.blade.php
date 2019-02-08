82ef787b340b6f0ec4c3e9e481d43acb61cd3fda
bonatua <bonatua.tas@gmail.com>
2019-02-08 13:56:01 +0700

    Initial Commit

---
+++ resources/views/companies/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+<div class="panel panel-default">
+    <div class="panel-heading">Panel</div>
+    <div class="panel-body">
+        <ul class="list-group">
+            <li class="list-group-item">Data</li>
+        </ul>
+    </div>
+</div>
